ff1b56cf04573ff423126f9581091c099bffb9a1
AlenaLoik <CrazyBeeLviv@gmail.com>
2019-06-19 15:52:11 +0300

    Updates

---
+++ precache-manifest.254274c153905ddaf00d09c83e07bb75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6eaeaa069fc1c81d00760523d7cc052",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "b01c7818a6e4229509c6",
+    "url": "/to-do-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bd0c9146313fb134b61e",
+    "url": "/to-do-app/static/js/2.7c0a1338.chunk.js"
+  },
+  {
+    "revision": "b01c7818a6e4229509c6",
+    "url": "/to-do-app/static/js/main.fae44c37.chunk.js"
+  },
+  {
+    "revision": "6cf4045563d5e41dd4b3",
+    "url": "/to-do-app/static/js/runtime~main.01361c46.js"
+  }
+]);
